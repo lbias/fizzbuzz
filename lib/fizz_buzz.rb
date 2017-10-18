8e8707a5228a3bfb1dc3e5a21e84474b6ad8cbd8
lbias <306508354@qq.com>
2017-10-18 19:10:25 +0800

    fizz_buzz#fizz_buzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,6 +1,10 @@
 def fizz_buzz(number)
-  if number % 3 == 0
+  if number % 3 == 0 and number % 5 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
     'fizz'
+  elsif number % 5 ==0
+    'buzz'
   else
     number
   end
